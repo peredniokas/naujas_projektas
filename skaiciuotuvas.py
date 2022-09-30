03580aabeb37065013906b87cb163f3996a7ff8c
perednioks <peredniokas23@gmail.com>
2022-09-30 08:20:03 +0300

    ivedimas

--- skaiciuotuvas.py
+++ skaiciuotuvas.py
@@ -7,27 +7,25 @@ from tkinter import *
 pagrindinis_langas = Tk()
 pagrindinis_langas.title('Skaiciuotuvas')
 
-def pasisveikinti():
-    ivesta = ivedimas1.get()
-    uzrasas2["text"] = (f"Labas, {ivesta}!")
-
+def mygtuko_ivedimas():
+    return
 # Laukų, mygtukų formavimas
 uzrasas1 = Label(pagrindinis_langas, text="")
 ivedimas1 = Entry(pagrindinis_langas)
-mygtukas1 = Button(pagrindinis_langas, text="1", )
-mygtukas2 = Button(pagrindinis_langas, text="2", )
-mygtukas3 = Button(pagrindinis_langas, text="3", )
-mygtukas4 = Button(pagrindinis_langas, text="4", )
-mygtukas5 = Button(pagrindinis_langas, text="5", )
-mygtukas6 = Button(pagrindinis_langas, text="6", )
-mygtukas7 = Button(pagrindinis_langas, text="7", )
-mygtukas8 = Button(pagrindinis_langas, text="8", )
-mygtukas9 = Button(pagrindinis_langas, text="9", )
-mygtukas10 = Button(pagrindinis_langas, text="0", )
-mygtukas11= Button(pagrindinis_langas, text="=", )
-mygtukas12= Button(pagrindinis_langas, text="+", )
+mygtukas1 = Button(pagrindinis_langas, text="1", command=mygtuko_ivedimas)
+mygtukas2 = Button(pagrindinis_langas, text="2", command=mygtuko_ivedimas)
+mygtukas3 = Button(pagrindinis_langas, text="3", command=mygtuko_ivedimas)
+mygtukas4 = Button(pagrindinis_langas, text="4", command=mygtuko_ivedimas)
+mygtukas5 = Button(pagrindinis_langas, text="5", command=mygtuko_ivedimas)
+mygtukas6 = Button(pagrindinis_langas, text="6", command=mygtuko_ivedimas)
+mygtukas7 = Button(pagrindinis_langas, text="7", command=mygtuko_ivedimas)
+mygtukas8 = Button(pagrindinis_langas, text="8", command=mygtuko_ivedimas)
+mygtukas9 = Button(pagrindinis_langas, text="9", command=mygtuko_ivedimas)
+mygtukas10 = Button(pagrindinis_langas, text="0", command=mygtuko_ivedimas)
+mygtukas11= Button(pagrindinis_langas, text="=", command=mygtuko_ivedimas)
+mygtukas12= Button(pagrindinis_langas, text="+", command=mygtuko_ivedimas)
 
-ivedimas1.bind("<Return>", lambda event: pasisveikinti())
+
 uzrasas2 = Label(pagrindinis_langas, text="")
 
 # Lango piešimas
