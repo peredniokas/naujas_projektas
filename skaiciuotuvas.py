2d7167ff17faf4f20e511d2731610d60db257177
perednioks <peredniokas23@gmail.com>
2022-09-30 14:09:04 +0300

    judam

--- skaiciuotuvas.py
+++ skaiciuotuvas.py
@@ -7,12 +7,14 @@ Skaiciuotuvas.title("Pragaro Skaiciuotuvas")
 input_frame = Frame(Skaiciuotuvas, width = 316, height = 50, bd = 3, highlightbackground = "Green", highlightcolor = "black", highlightthickness = 1)
 input_frame.pack(side = TOP)
 
-input_field = Entry(input_frame, font = ('arial', 18, 'bold'), textvariable = "input_text", width = 50, bg = "#eee", bd = 0, justify = RIGHT)
-input_field.grid(row = 0, column = 0)
-input_field.pack(ipady = 10)
+ivedimo_ekranas = Entry(input_frame, font = ('arial', 18, 'bold'), textvariable = "input_text", width = 50, bg = "#eee", bd = 0, justify = RIGHT)
+ivedimo_ekranas.grid(row = 0, column = 0)
+ivedimo_ekranas.pack(ipady = 10)
 
 btns_frame = Frame(Skaiciuotuvas, width = 312, height = 272.5, bg = "grey")
 btns_frame.pack()
+
+
 
 
 
