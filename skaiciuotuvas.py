eb7e6e410c4de180d43ae039a4595d5bb8cfa4d4
perednioks <peredniokas23@gmail.com>
2022-09-30 09:03:35 +0300

    kazkas

--- skaiciuotuvas.py
+++ skaiciuotuvas.py
@@ -13,21 +13,22 @@ pagrindinis_langas.title('Skaiciuotuvas')
 ivedimas1 = Entry(pagrindinis_langas, width=35, borderwidth=5)
 ivedimas1.grid(row=0, column=0, columnspan=3)
 
-def mygtuko_ivedimas():
-    return
+def mygtuko_ivedimas(number):
+    ivedimas1.delete(0, END)
+    ivedimas1.insert(0, number)
 
-mygtukas1 = Button(pagrindinis_langas, text="1", padx=40, pady=20, command=mygtuko_ivedimas)
-mygtukas2 = Button(pagrindinis_langas, text="2", padx=40, pady=20, command=mygtuko_ivedimas)
-mygtukas3 = Button(pagrindinis_langas, text="3", padx=40, pady=20, command=mygtuko_ivedimas)
-mygtukas4 = Button(pagrindinis_langas, text="4", command=mygtuko_ivedimas)
-mygtukas5 = Button(pagrindinis_langas, text="5", command=mygtuko_ivedimas)
-mygtukas6 = Button(pagrindinis_langas, text="6", command=mygtuko_ivedimas)
-mygtukas7 = Button(pagrindinis_langas, text="7", command=mygtuko_ivedimas)
-mygtukas8 = Button(pagrindinis_langas, text="8", command=mygtuko_ivedimas)
-mygtukas9 = Button(pagrindinis_langas, text="9", command=mygtuko_ivedimas)
-mygtukas0 = Button(pagrindinis_langas, text="0", command=mygtuko_ivedimas)
-mygtukas_plius= Button(pagrindinis_langas, text="+", command=mygtuko_ivedimas)
-mygtukas_lygu= Button(pagrindinis_langas, text="=", command=mygtuko_ivedimas)
+mygtukas1 = Button(pagrindinis_langas, text="1", padx=40, pady=20, command=lambda: mygtuko_ivedimas(1))
+mygtukas2 = Button(pagrindinis_langas, text="2", padx=40, pady=20, command=lambda: mygtuko_ivedimas(2))
+mygtukas3 = Button(pagrindinis_langas, text="3", padx=40, pady=20, command=lambda: mygtuko_ivedimas(3))
+mygtukas4 = Button(pagrindinis_langas, text="4", padx=40, pady=20, command=lambda: mygtuko_ivedimas(4))
+mygtukas5 = Button(pagrindinis_langas, text="5", padx=40, pady=20, command=lambda: mygtuko_ivedimas(5))
+mygtukas6 = Button(pagrindinis_langas, text="6", padx=40, pady=20, command=lambda: mygtuko_ivedimas(6))
+mygtukas7 = Button(pagrindinis_langas, text="7", padx=40, pady=20, command=lambda: mygtuko_ivedimas(7))
+mygtukas8 = Button(pagrindinis_langas, text="8", padx=40, pady=20, command=lambda: mygtuko_ivedimas(8))
+mygtukas9 = Button(pagrindinis_langas, text="9", padx=40, pady=20, command=lambda: mygtuko_ivedimas(9))
+mygtukas0 = Button(pagrindinis_langas, text="0", padx=40, pady=20, command=lambda: mygtuko_ivedimas(0))
+mygtukas_plius= Button(pagrindinis_langas, text="+", command=lambda: mygtuko_ivedimas())
+mygtukas_lygu= Button(pagrindinis_langas, text="=", command=lambda: mygtuko_ivedimas())
 
 
 
@@ -42,8 +43,8 @@ mygtukas7.grid(row=1, column=0)
 mygtukas8.grid(row=1, column=1)
 mygtukas9.grid(row=1, column=2)
 mygtukas0.grid(row=4, column=2)
-mygtukas_plius.grid(row=1, column=2)
+mygtukas_plius.grid(row=5, column=0)
 
-mygtukas_lygu.grid(row=1, column=2)
+mygtukas_lygu.grid(row=5, column=1)
 
 pagrindinis_langas.mainloop()
